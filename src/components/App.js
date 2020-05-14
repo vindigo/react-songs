de81c19ae5a3d16253de9ca5c63753bcd82fc158
Richard Frausto <vindigo@gmail.com>
2020-05-13 18:15:33 -0700

    added SongDetail component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import './App.css';
 import SongList from './SongList';
+import SongDetail from './SongDetail';
 
 const App = () => {
   return (
-    <div>
-      <SongList />
+    <div className="ui container grid">
+      <div className="ui row">
+        <div className="column eight wide">
+          <SongList />
+        </div>
+        <div className="column eight wide song-detail">
+          <SongDetail />
+        </div>
+      </div>
     </div>
   )
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,16 +1,19 @@
-import { combineReducers } from 'redux';
+import { combineReducers, bindActionCreators } from 'redux';
 
 const songsReducer = () => {
   return [
-    { title: 'Push the Little Daisies', duration: '2:55' },
-    { title: 'Push the tempo', duration: '4:28' },
-    { title: 'Hobo Humpin Slobo Babe', duration: '3:57' },
-    { title: 'Close to the Edit', duration: '4:11' }
+    { title: 'Beat Box', duration: '4:50', artist: 'Art of Noise' },
+    { title: 'Push the tempo', duration: '4:28', artist: 'Fatboy Slim' },
+    { title: 'Hobo Humpin Slobo Babe', duration: '3:57', artist: 'Whale' },
+    { title: 'Little L', duration: '3:56', artist: 'Jamiroquai' },
+    { title: 'Square Dance Rap', duration: '4:18', artist: 'Sir Mix-a-Lot' },
+    { title: 'Here We Go', duration: '3:57', artist: 'Stakka Bo' },
+    { title: 'Groove is in the Heart', duration: '4:50', artist: 'Deee-lite' }
   ];
 };
 
 const selectedSongReducer = ( selectedSong = null, action ) => {
-  if ( action.type === 'SELECTED_SONG' ){
+  if ( action.type === 'SONG_SELECTED' ){
     return action.payload;
   }
 
